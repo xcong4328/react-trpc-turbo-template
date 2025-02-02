c02e65139792a52395c6a26fd18377df0e8f404a
x_cong <x_cong@oec.co.jp>
2025-02-02 18:15:18 +0900

    sample login and MUI snackbar

--- apps/api/src/router/index.ts
+++ apps/api/src/router/index.ts
@@ -1,9 +1,13 @@
 import { router } from '@api/trpc';
 import { helloRouter } from '@api/procedures/hello';
 import { createUser } from '../procedures/createUser';
+import { loginRouter } from '../procedures/auth/loginRouter';
 import { getUsers } from '@api/procedures/getUsers';
 
 export const appRouter = router({
+  auth: {
+    login: loginRouter,
+  },
   hello: {
     get: helloRouter.get,
   },
--- apps/web/src/components/features/auth/login/index.lazy.tsx
+++ apps/web/src/components/features/auth/login/index.lazy.tsx
@@ -1,13 +1,18 @@
 import { createLazyRoute } from '@tanstack/react-router';
-import { Box, Button, Container, TextField, Typography } from '@mui/material';
+import { Box, Button, Container, IconButton, Snackbar, SnackbarCloseReason, TextField, Typography } from '@mui/material';
 import { useForm, SubmitHandler } from "react-hook-form";
 import { z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { trpc } from '@/utils/trpc';
+import React from 'react';
+import { TRPCClientError } from '@trpc/client';
+import CloseIcon from '@mui/icons-material/Close';
+import CustomSnacker from '@/components/common/Snackbar';
 
 // Define Zod schema
 const authSchema = z.object({
   userName: z.string().min(1, "User name is required"),
-  passWord: z.string().min(1, "Password is required"),
+  password: z.string().min(1, "Password is required"),
 });
 
 // Infer TypeScript type from Zod schema
@@ -22,7 +27,29 @@ export default function LoginComponent() {
     resolver: zodResolver(authSchema),
   });
 
-  const onSubmit: SubmitHandler<Auth> = (data) => console.log(data);
+  const [openSnackbar, setOpenSnackbar] = React.useState(false)
+  const [errorMessage, setErrorMessage] = React.useState('')
+  console.log("⛳️ log ~ LoginComponent log ~ errorMessage: ", errorMessage)
+
+  // const {mutate} = trpc.auth.login.useMutation()
+  const loginMutation = trpc.auth.login.useMutation();
+
+  const onSubmit: SubmitHandler<Auth> = async (data) => {
+    console.log(data);
+    // mutate(data)
+    try{
+      const response = await loginMutation.mutateAsync(data);
+      localStorage.setItem("token", response.token)
+      console.log("⛳️ log ~ LoginComponent log ~ response: ", response)
+    }catch(error){
+      if(error instanceof TRPCClientError){
+        console.log("⛳️ log ~ constonSubmit:SubmitHandler<Auth>= log ~ error: ", error.message)
+        setErrorMessage(error.message)
+        setOpenSnackbar(true);
+      }
+    }
+  }
+
 
   return (
       <Container
@@ -45,20 +72,24 @@ export default function LoginComponent() {
       </Box>
       <Box sx={{ mt: 2 }}>
         <TextField
-          {...register("passWord")}
+          {...register("password")}
           type="password"
           autoComplete="new-password"
           variant="outlined"
           label="Password"
           sx={{ width: '100%' }}
-          error={!!errors.passWord}
-          helperText={errors.passWord?.message}
+          error={!!errors.password}
+          helperText={errors.password?.message}
         />
       </Box>
-
       <Box sx={{ mt: 2 }}>
         <Button variant="contained" type="submit">Login</Button>
       </Box>
+      <CustomSnacker open={openSnackbar} message={errorMessage} onClose={() => setOpenSnackbar(false)} />
+
+      {loginMutation.error && (
+        <Typography color="error">{loginMutation.error.message}</Typography>
+      )}
     </Container>
   );
 }
